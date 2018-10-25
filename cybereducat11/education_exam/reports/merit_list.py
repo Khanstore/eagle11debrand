ae93d26e58ef5a836352238c4aacb7fc8b5098ea
Khanstore <dmashraf@gmail.com>
2018-10-25 16:15:40 +0600

    meritlist

--- cybereducat11/education_exam/reports/merit_list.py
+++ cybereducat11/education_exam/reports/merit_list.py
@@ -12,32 +12,82 @@ class examEvaluation(models.AbstractModel):
     def get_merit_list(self,object):
         list=[]
         stu=[]
-        scor=[]
+        total_scor=[]
         exa=[]
+        section=[]
         merit_class=[]
         merit_section=[]
+        index=1
+        student_list=[]
         for exam in object.exams:
-            student_list = self.env['education.class.history'].search([('level.id', '=', object.level.id)])
+            exa=[]
+            scor=[]
+            merit_class=[]
+            merit_section=[]
+            if index==1:
+                student_list = self.env['education.class.history'].search([('level.id', '=', object.level.id)])
             for student in student_list:
                 total=0
                 mark_line = self.env['results.subject.line'].search(
                     [('student_id', '=', student.id), ('exam_id', '=', exam.id)])
                 for line in mark_line:
-                    total=total+line.mark_scored
+                    if line.mark_scored:
+                        total=total+line.mark_scored
+                section.append(student.section)
                 stu.append(student)
                 exa.append(exam)
                 scor.append(total)
                 merit_class.append(0)
                 merit_section.append(0)
-            data={'student':stu,"exam":exa,'Score':scor,'merit_class':merit_class,'merit_section':merit_section}
-        df= pd.DataFrame(data)
+            if index==1:
+                data={'student':stu,'section':section,"exam"+ str(index) :exa,
+                      'Score'+ str(index) :scor,'merit_class'+ str(index) :merit_class,'merit_section'+ str(index) :merit_section,
+                      'Score': scor, 'merit_class': merit_class,
+                      'merit_section' : merit_section}
+                df= pd.DataFrame(data)
+            else:
+                df.insert(3, 'exam'+str(index), exa, allow_duplicates=False)
+                df.insert(4, 'Score'+str(index), scor, allow_duplicates=False)
+                df.insert(4, 'merit_class'+str(index), merit_class, allow_duplicates=False)
+                df.insert(4, 'merit_section'+str(index), merit_section, allow_duplicates=False)
+
+            result = df.sort_values(by=['Score'+str( index) ], ascending=False)
+            result=result.reset_index(drop=True)
+            for i in range(0,len(result)):
+                df.loc[df[ 'student' ] == result.at[i,'student'], 'merit_class'+ str(index) ] = i+1
+                if index>1:
+                    df.loc[df[ 'student' ] == result.at[i,'student'], 'Score' ] = result.at[i,'Score']+result.at[i,'Score'+str(index)]
+                # result.at[i,'merit_class']=i+1
+                i=i+1
+            section_list=df.section.unique()
+            for rec in section_list:
+                df1 = df[(df['section'] ==rec )]
+                result = df1.sort_values(by=['Score'+str (index) ], ascending=False)
+                result = result.reset_index(drop=True)
+                for i in range(0, len(result)):
+                    df.loc[df['student'] == result.at[i, 'student'], 'merit_section'+str (index) ] = i + 1
+                    # result.at[i, 'merit_section'] = i + 1
+                    i = i + 1
+            index = index + 1
         result = df.sort_values(by=['Score'], ascending=False)
-        result=result.reset_index(drop=True)
-        for i in range(0,len(result)):
-            result.at[i,'merit_class']=i+1
-            i=i+1
-        print(result)
-        return result
+        result = result.reset_index(drop=True)
+        for i in range(0, len(result)):
+            df.loc[df['student'] == result.at[i, 'student'], 'merit_class'] = i + 1
+
+            # result.at[i,'merit_class']=i+1
+            i = i + 1
+        section_list = df.section.unique()
+        for rec in section_list:
+            df1 = df[(df['section'] == rec)]
+            result = df1.sort_values(by=['Score'], ascending=False)
+            result = result.reset_index(drop=True)
+            for i in range(0, len(result)):
+                df.loc[df['student'] == result.at[i, 'student'], 'merit_section'] = i + 1
+                # result.at[i, 'merit_section'] = i + 1
+                i = i + 1
+
+
+        return df
 
 
 
